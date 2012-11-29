6e1900ebc84b4e384dd427eac471509a9b86d533
Pedro Rodriguez <pjrod@fullsail.edu>
2012-11-29 10:00:53 -0500

    updated js file

--- rodriguez_pedro_project2.js
+++ rodriguez_pedro_project2.js
@@ -1,10 +1,17 @@
-alert("JavaScript works!")
-
 //Pedro J. Rodriguez
 //Project 2
 //SDI 1212
 
 // function
+var coastieCrew = "Crew",
+    coastie = "Pedro"
+;
+var smallBoat = "25 RBS",
+    bigBoat = "41 UTB"
+;
+var hoursToLocation = 2;
+
+
 var division = function(firstNumber,secondNumber){
 	var outcome = firstNumber/secondNumber;
 	
@@ -18,3 +25,7 @@ var divOutcome = division(10,5);
 
 console.log("The answer is " + divOutcome);
 
+var getArea = function () {
+	console.log ("getArea Fired!");
+};
+getArea();
