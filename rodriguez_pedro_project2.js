6f8cfc2e534c857c15ef9bf48d28b5c1c12c288f
Pedro Rodriguez <pjrod@fullsail.edu>
2012-11-29 10:47:39 -0500

    updated js file

--- rodriguez_pedro_project2.js
+++ rodriguez_pedro_project2.js
@@ -2,15 +2,22 @@
 //Project 2
 //SDI 1212
 
-// function
+
 var coastieCrew = "Crew",
     coastie = "Pedro"
 ;
 var smallBoat = "25 RBS",
     bigBoat = "41 UTB"
 ;
-var hoursToLocation = 2;
+var hoursToLocation = 2; 
+var windSpeed = 25; // local wind speed can change to affect outcome of story
+var crewGarScore = 24; // Gar Model Score can chage to affect status of situation 
+var searchPattern = "Expanding Square Search";
 
+var garModel = ["Green", "Amber", "Red"];
+
+//function
+//Main Code
 
 var division = function(firstNumber,secondNumber){
 	var outcome = firstNumber/secondNumber;
